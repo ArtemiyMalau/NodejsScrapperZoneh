ddf623d0db0fdabc6bbe4d34bcb939ecc9ac38b3
ArtemiyMalau <ya.fantic2012@yandex.ru>
2021-09-24 20:09:05 +0300

    Remove unnecessary python scripts

--- py/config.py
+++
@@ -1,5 +0,0 @@
-from json import loads
-
-config_path = "./config.json"
-with open(config_path, "r") as file:
-	config = loads(file.read())
--- py/database.py
+++
@@ -1,7 +0,0 @@
-import mysql.connector
-
-from config import config
-
-connection = mysql.connector.connect(**config["db"])
-
-cursor = connection.cursor(dictionary=True)
